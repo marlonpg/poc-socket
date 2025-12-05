d5075724a211ef9914f68fb6dab317a0998984ad
Marlon Gamba <marlonpgamba@gmail.com>
2025-12-04 22:00:06 -0300

    Updating java version

--- run-client.bat
+++ run-client.bat
@@ -1,3 +1,5 @@
 @echo off
+set JAVA_HOME=C:\Users\gamba\.jdks\corretto-25.0.1
+set PATH=%JAVA_HOME%\bin;%PATH%
 echo Starting client...
 java Client
--- run-server.bat
+++ run-server.bat
@@ -1,4 +1,6 @@
 @echo off
+set JAVA_HOME=C:\Users\gamba\.jdks\corretto-25.0.1
+set PATH=%JAVA_HOME%\bin;%PATH%
 echo Compiling Java files...
 javac *.java
 echo Starting server...
